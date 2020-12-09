3cd165d40c0b4d4d56de956e42d977c8d3bec54d
guxal <jonathanacp93@gmail.com>
2020-12-09 00:23:04 +0000

    update code 0-lockboxes

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -3,32 +3,29 @@
 
 
 def reUnlock(boxes, inc, keys):
-    
+    """
+    Returns: True if the all boxes is Unlock else False
+    Parameters:
+        boxes: is a list of lists
+        inc: index of lists
+        keys: keys open box
+    """
     if (len(boxes) > inc):
         if(inc in keys):
             keys += boxes[inc]
-        #print("box: {}, INDEX: {}".format(boxes[inc], inc))
-        #print("keys: {}".format(keys))
         reUnlock(boxes, inc + 1, keys)
-    
-    if (inc < len(boxes)):
-        if (inc in keys):
+
+    if (inc < len(boxes) and inc in keys):
             keys += boxes[inc]
-    #print("inc: {}".format(inc))
-    #print("keys: {}".format(keys))
-    #print("len: {}".format(set(keys) | {0}))
-    
-    return len(list(set(keys)| {0})) == len(boxes)
-    
+
+    return len(list(set(keys) | {0})) == len(boxes)
 
 
 def canUnlockAll(boxes):
     """
     Returns:
-        True  if the all boxes is Unlock else False
+        True if the all boxes is Unlock else False
     Parameters:
         boxes: is a list of lists
     """
-    #print("len boxes {}".format(len(boxes)))
-    #print("init keys {}".format(boxes[0]))
     return reUnlock(boxes, 1, boxes[0])
