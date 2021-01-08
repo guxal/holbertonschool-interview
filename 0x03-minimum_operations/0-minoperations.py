e9af91bf1ee84f2b836d8d26c346c5ca9e712b6b
guxal <jonathanacp93@gmail.com>
2021-01-08 01:49:41 +0000

    add new logic

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -6,7 +6,7 @@
 """
 
 
-def minOperationsRecursive(n, h):
+def minOperationsRecursive(n, h, hh):
     """
       Function Recursive that calculates the fewest
       number of operations needed to
@@ -17,11 +17,11 @@ def minOperationsRecursive(n, h):
         return 0
 
     if (n >= h * 2):
-        val = minOperationsRecursive(n, h * 2)
+        val = minOperationsRecursive(n, h * 2, hh * 2)
         if val != -1:
             return val + 2
-    if (n >= h + h/2):
-        val = minOperationsRecursive(n, h + h/2)
+    if (n >= h + hh/2):
+        val = minOperationsRecursive(n, h + hh/2, hh)
         if val != -1:
             return val + 1
 
@@ -37,4 +37,4 @@ def minOperations(n):
     if n <= 1:
         return 0
 
-    return minOperationsRecursive(n, 1)
+    return minOperationsRecursive(n, 1, 1)
