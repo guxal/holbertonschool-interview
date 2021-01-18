33a9dd745ccc752bd3efe34c98dc90700e40430e
guxal <jonathanacp93@gmail.com>
2021-01-18 03:15:16 +0000

    update code

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -23,48 +23,56 @@ static void print_grid(int grid[3][3])
 }
 
 /**
- * sandpiles_toop - recursive derrame
- * @grid1: Left 3x3 grid
- * @derrame: is true repeat
+ * add - add the pile
+ * @grid1: grid1
+ * @grid2: grid2
+ * Return: void
  */
-void sandpiles_toop(int grid1[3][3], int derrame)
+void add(int grid1[3][3], int grid2[3][3])
 {
-	int i, j;
+	size_t i, j;
 
-	if (derrame == 1)
-	{
-		printf("=\n");
-		print_grid(grid1);
-		derrame = 0;
-		for (i = 0; i < 3; i++)
-		{
-			for (j = 0; j < 3; j++)
-			{
-				if (grid1[i][j] > 3)
-				{
-					grid1[i][j] = grid1[i][j] - 4;
-					if (i + 1 < 3)
-						grid1[i + 1][j] = grid1[i + 1][j] + 1;
-					if (i - 1 >= 0)
-						grid1[i - 1][j] = grid1[i - 1][j] + 1;
-					if (j + 1 < 3)
-						grid1[i][j + 1] = grid1[i][j + 1] + 1;
-					if (j - 1 >= 0)
-						grid1[i][j - 1] = grid1[i][j - 1] + 1;
-				}
-			}
-		}
-		for (i = 0; i < 3; i++)
-		{
-			for (j = 0; j < 3; j++)
-			{
-				if (grid1[i][j] > 3)
-					derrame = 1;
-			}
-		}
-		if (derrame == 1)
-			sandpiles_toop(grid1, derrame);
-	}
+	for (i = 0; i < 3; i++)
+		for (j = 0; j < 3; j++)
+			grid1[i][j] += grid2[i][j], grid2[i][j] = 0;
+}
+
+/**
+ * sandpiles_stable - stabilize sandpiles
+ * @grid1: Left 3x3 grid
+ * @grid2: Right 3x3 grid
+ */
+void sandpiles_stable(int grid1[3][3], int grid2[3][3])
+{
+	size_t i, j;
+
+	printf("=\n");
+	print_grid(grid1);
+	for (i = 0; i < 3; i++)
+		for (j = 0; j < 3; j++)
+			if (grid1[i][j] > 3)
+				(i < 2) ? grid2[i + 1][j] += 1 : 0,
+				(j < 2) ? grid2[i][j + 1] += 1 : 0,
+				(i > 0) ? grid2[i - 1][j] += 1 : 0,
+				(j > 0) ? grid2[i][j - 1] += 1 : 0,
+				grid1[i][j] -= 4;
+	add(grid1, grid2);
+}
+
+/**
+ * is_unstable - is unstable true or false
+ * @grid: the input
+ * Return: 1 is stable 0 is unstable
+ */
+int is_unstable(int grid[3][3])
+{
+	size_t i, j;
+
+	for (i = 0; i < 3; i++)
+		for (j = 0; j < 3; j++)
+			if (grid[i][j] > 3)
+				return (0);
+	return (1);
 }
 
 /**
@@ -74,18 +82,10 @@ void sandpiles_toop(int grid1[3][3], int derrame)
  */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-	int i, j, derrame;
 	/* sum */
-	for (i = 0; i < 3; i++)
+	add(grid1, grid2);
+	while (!is_unstable(grid1))
 	{
-		for (j = 0; j < 3; j++)
-		{
-			grid1[i][j] = grid1[i][j] + grid2[i][j];
-			if (grid1[i][j] > 3)
-				derrame = 1;
-		}
+		sandpiles_stable(grid1, grid2);
 	}
-	/* derrame */
-
-	sandpiles_toop(grid1, derrame);
 }
