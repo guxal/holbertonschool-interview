e6375294f5a3110f56086c4c169d538c85d42324
guxal <jonathancp93@gmail.com>
2021-03-22 19:58:31 +0000

    b code

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -1,44 +1,74 @@
 #!/usr/bin/python3
-''' N queens problem '''
+"""N Queens"""
 import sys
-from copy import deepcopy
 
 
-def is_valid(board, row):
-    for i in range(row):
-        diff = abs(board[i][1] - board[row][1])
-        if diff == 0 or diff == row - i:
+def printss(board):
+    """
+    Prints the coordinates row and column for the position of
+    each N queen in the posible solution
+    """
+
+    solve = []
+    for i in range(len(board)):
+        for j in range(len(board)):
+            if board[i][j] == 1:
+                solve.append([i, j])
+    print(solve)
+
+
+def isSafe(board, row, col, n):
+    """
+    Checks if a queen can be placed on board
+    """
+
+    for c in range(col):
+        if board[row][c] == 1:
             return False
+
+    for r, c in zip(range(row, -1, -1),
+                    range(col, -1, -1)):
+        if board[r][c] == 1:
+            return False
+
+    for r, c in zip(range(row, n, 1),
+                    range(col, -1, -1)):
+        if board[r][c] == 1:
+            return False
+
     return True
 
 
-def n_queens(res, board, row, n):
-    if row == n:
-        # Deep copy is needed because board is a 2D list
-        # or you can just print the board if task is to dislay solutions
-        res.append(deepcopy(board))
-    else:
-        for col in range(n):
-            board[row][1] = col
-            if is_valid(board, row):
-                n_queens(res, board, row + 1, n)
+def solution(board, col, n):
+    """Solve the n queen problem using Backtracking"""
+
+    if col == n:
+        printss(board)
+        return True
+
+    c = False
+    for i in range(n):
+        if isSafe(board, i, col, n):
+            board[i][col] = 1
+            c = solution(board, col + 1, n) or c
+            board[i][col] = 0
+    return c
 
 
-if __name__ == '__main__':
-    if len(sys.argv) != 2:
+if __name__ == "__main__":
+    """Starting the program"""
+    if not len(sys.argv) == 2:
         print("Usage: nqueens N")
-        exit(1)
-    try:
-        n = int(sys.argv[1])
-    except Exception:
+        sys.exit(1)
+
+    if not (sys.argv[1]).isdigit():
         print("N must be a number")
-        exit(1)
+        sys.exit(1)
+
+    n = int(sys.argv[1])
     if n < 4:
         print("N must be at least 4")
-        exit(1)
+        sys.exit(1)
 
-    res = []
-    board = [[i, -1] for i in range(n)]
-    n_queens(res, board, 0, n)
-    for i in res:
-        print(i)
+    board = [[0 for i in range(n)] for j in range(n)]
+    solution(board, 0, n)
