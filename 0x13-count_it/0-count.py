26ea132fddbf61f74d00e7d4af0c21fd3e506216
guxal <jonathanacp93@gmail.com>
2021-05-07 12:54:15 +0000

    add new logic

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -1,58 +1,36 @@
 #!/usr/bin/python3
-"""0-count module"""
-
-import collections
+"""
+Request the top ten hot posts
+"""
 import requests
 
 
-def fill_list(subreddit, hot_list=[], after=None):
-    """Create list of words in hot titles of specified subreddit
-    Args:
-        subreddit: string containing subreddit to search
-        hot_list: list to populate
-        after: pagination for next entry
-    Returns: populated list or None of failure
+def count_words(subreddit, word_list, after=None, my_dict={}):
     """
-    req = requests.get("https://www.reddit.com/r/{}/hot.json?after={}".
-                       format(subreddit, after),
-                       headers={"User-agent": "agent"},
-                       allow_redirects=False)
-    if req.status_code != 200:
-        return None
-    after = req.json().get("data").get("after")
-    if after is None:
-        return hot_list
-    for i in req.json().get("data").get("children"):
-        for word in i.get("data").get("title").split():
-            hot_list.append(word.lower())
-    return fill_list(subreddit, hot_list, after)
-
-
-def count_words(subreddit, word_list):
-    """Finds occurences of specified keywords in a given subreddit
-    Prints keyword along with their occurrences in descending order. Keywords
-    with no matches are skipped
-    Args:
-        subreddit: string containing subreddit to search
-        word_list: list of keywords to search for
-    Returns: OrderedDict with keys as keywords and occurrences as values or
-    None on request failure
+    Gets the top ten hot post of a subreddit
     """
-    if subreddit is None or subreddit == "" or word_list is None:
-        return None
-    hot_list = fill_list(subreddit)
-    if hot_list is None:
-        return None
-    all_cnt = collections.Counter(hot_list)
-    filtered_cnt = {}
-    for word in word_list:
-        word_l = word.lower()
-        if all_cnt[word_l] > 0:
-            if word in filtered_cnt:
-                filtered_cnt[word] += filtered_cnt[word]
-            else:
-                filtered_cnt[word] = all_cnt[word_l]
-    for k, v in sorted(filtered_cnt.items(),
-                       key=lambda item: item[1], reverse=True):
-        print("{}: {}".format(k, v))
-    return filtered_cnt
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    payload = {'after': after}
+    response = requests.get(url,
+                            allow_redirects=False,
+                            params=payload,
+                            headers={'User-Agent': 'Pear'})
+    if response and response.status_code == 200:
+        post_list = response.json().get('data').get('children')
+        for children in post_list:
+            title1 = children.get('data').get('title')
+            for word in word_list:
+                try:
+                    my_dict[word] += title1.lower().split().count(word.lower())
+                except KeyError:
+                    my_dict[word] = title1.lower().split().count(word.lower())
+        after = response.json().get('data').get('after')
+        if (after is None):
+            for key, val in sorted(my_dict.items(),
+                                   key=lambda x: (-1*x[1], -1*x[0])):
+                if (val != 0):
+                    print("{}: {}".format(key.lower(), val))
+            return
+        return count_words(subreddit, word_list, after, my_dict)
+    else:
+        return
