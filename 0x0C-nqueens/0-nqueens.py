ded758a24c21adb177e5aff1cd80f6ee3b17b3db
guxal <jonathanacp93@gmail.com>
2021-07-26 04:10:33 +0000

    new 0-nqueens.py

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -1,74 +1,45 @@
 #!/usr/bin/python3
-"""N Queens"""
+
 import sys
 
 
-def printss(board):
+def nqueens(queens, xy_diff, xy_sum):
+    """Finds all valid queen column positions that don't attack each other
+    Args:
+        queens: columns occupied by queens
+        xy_diff: positive slope diagonals occupied by queens
+        xy_sum: negative slope diagonals occupied by queens
     """
-    Prints the coordinates row and column for the position of
-    each N queen in the posible solution
-    """
-
-    solve = []
-    for i in range(len(board)):
-        for j in range(len(board)):
-            if board[i][j] == 1:
-                solve.append([i, j])
-    print(solve)
+    p = len(queens)
+    if p == n:
+        queen_col.append(queens)
+        return None
+    for q in range(n):
+        if q not in queens and p - q not in xy_diff and p + q not in xy_sum:
+            nqueens(queens + [q], xy_diff + [p - q], xy_sum + [p + q])
 
 
-def isSafe(board, row, col, n):
-    """
-    Checks if a queen can be placed on board
-    """
-
-    for c in range(col):
-        if board[row][c] == 1:
-            return False
-
-    for r, c in zip(range(row, -1, -1),
-                    range(col, -1, -1)):
-        if board[r][c] == 1:
-            return False
-
-    for r, c in zip(range(row, n, 1),
-                    range(col, -1, -1)):
-        if board[r][c] == 1:
-            return False
-
-    return True
-
-
-def solution(board, col, n):
-    """Solve the n queen problem using Backtracking"""
-
-    if col == n:
-        printss(board)
-        return True
-
-    c = False
-    for i in range(n):
-        if isSafe(board, i, col, n):
-            board[i][col] = 1
-            c = solution(board, col + 1, n) or c
-            board[i][col] = 0
-    return c
-
-
-if __name__ == "__main__":
-    """Starting the program"""
-    if not len(sys.argv) == 2:
+def parse_argv():
+    """Checks for usage errors"""
+    if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-
-    if not (sys.argv[1]).isdigit():
+    if not sys.argv[1].isdigit():
         print("N must be a number")
         sys.exit(1)
-
     n = int(sys.argv[1])
     if n < 4:
         print("N must be at least 4")
         sys.exit(1)
+    return n
 
-    board = [[0 for i in range(n)] for j in range(n)]
-    solution(board, 0, n)
+
+if __name__ == "__main__":
+    n = parse_argv()
+    queen_col = []
+    nqueens([], [], [])
+    for row in range(len(queen_col)):
+        queen_pos = []
+        for col in range(len(queen_col[row])):
+            queen_pos.append([col, queen_col[row][col]])
+        print(queen_pos)
