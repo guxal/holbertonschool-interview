76abad2103736bafe532883f9fbec887760d3756
guxal <jonathanacp93@gmail.com>
2021-11-19 02:58:48 +0000

    refactor: change logic

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -3,19 +3,19 @@
 
 
 def island_perimeter(grid):
-    island = [i for i in [[v, idx, idx2]
-              if v == 1 else None for idx, _ in enumerate(grid)
-              for idx2, v in enumerate(_)] if i]
+    """  returns the perimeter  """
 
-    if (len(island) == 0):
-        return 0
+    def edges(matrix):
+        """ detect number of edges"""
+        count = 0
+        for row in matrix:
+            row = [0] + row + [0]
+            for i in range(1, len(row)):
+                count += row[i] != row[i-1]
+        return count
 
-    max_h = [v[1] for v in island]
-    min_h = [v[1] for v in island]
-    max_w = [v[2] for v in island]
-    min_w = [v[2] for v in island]
-
-    sum_h = (max(max_h) - min(min_h)) + 1
-    sum_w = (max(max_w) - min(min_w)) + 1
-
-    return (sum_h * 2) + (sum_w * 2)
+    gridTwo = [[] for _ in range(len(grid[0]))]
+    for row in grid:
+        for i, item in enumerate(row):
+            gridTwo[i].append(item)
+    return edges(grid) + edges(gridTwo)
